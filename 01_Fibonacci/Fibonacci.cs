0f8084e9cf12c77095de31b8f71be7e85e99234d
Gabriel Pimenta <gabrielrpimenta@gmail.com>
2023-03-28 11:18:19 -0300

    create fibonacci sequence program

---
+++ 01_Fibonacci/Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a posição desejada na sequência de Fibonacci: ");
+            int posicao = int.Parse(Console.ReadLine());
+            
+            int numero = Fib(posicao);
+
+            Console.WriteLine($"O número da sequência de Fibonacci na posição {posicao} é {numero}");
+
+            Console.ReadKey();
+        }
+        
+        static int Fib(int posicao)
+        {
+            if (posicao == 0)
+                return 0;
+            else if (posicao == 1)
+                return 1;
+            else
+                return Fib(posicao - 1) + Fib(posicao - 2);
+        }
+    }
+}
